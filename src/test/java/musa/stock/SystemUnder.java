8a193ebe870aeb2ef3b1aa73a877c15d8186eadb
Urs Metz <urs.metz@gmx.de>
2020-12-14 23:01:06 +0100

    more tests

--- src/test/java/musa/stock/SystemUnder.java
+++ src/test/java/musa/stock/SystemUnder.java
@@ -15,7 +15,17 @@ class SystemUnder {
         if (values.profibility == 0) {
             return;
         }
+        if (values.profibility == 1) {
+            consumer.accept(new MqttConsumer.Message("some/led/0/rgb", Color_White, false));
+            return;
+        }
+
 
         consumer.accept(new MqttConsumer.Message("some/led/0/rgb", Color_White, false));
+        consumer.accept(new MqttConsumer.Message("some/led/1/rgb", Color_White, false));
+        consumer.accept(new MqttConsumer.Message("some/led/2/rgb", Color_White, false));
+        consumer.accept(new MqttConsumer.Message("some/led/3/rgb", Color_White, false));
+
+
     }
 }
--- src/test/java/musa/stock/SystemUnderTest.java
+++ src/test/java/musa/stock/SystemUnderTest.java
@@ -1,5 +1,6 @@
 package musa.stock;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -27,9 +28,29 @@ public class SystemUnderTest {
         TestConsumer consumer = new TestConsumer();
         new SystemUnder(consumer).nike(values);
 
-        MqttConsumer.Message message = consumer.sendMessages.get(0);
-        assertEquals("some/led/0/rgb", message.getTopic());
-        assertEquals(SystemUnder.Color_White, message.getPayload());
+        assertMessage(consumer, 0, SystemUnder.Color_White);
+    }
+
+    @Test
+    void activatesAllLedsInFirstRingForProfibility100() {
+        Values values = new Values();
+        values.profibility = 100;
+        values.amount = 0;
+
+        TestConsumer consumer = new TestConsumer();
+        new SystemUnder(consumer).nike(values);
+
+        assertMessage(consumer, 0, SystemUnder.Color_White);
+        assertMessage(consumer, 1, SystemUnder.Color_White);
+        assertMessage(consumer, 2, SystemUnder.Color_White);
+        assertMessage(consumer, 3, SystemUnder.Color_White);
+    }
+
+    private void assertMessage(TestConsumer consumer, int index, String color) {
+        Assertions.assertTrue(consumer.sendMessages.size() >= index + 1, "not enough messages present");
+        MqttConsumer.Message message = consumer.sendMessages.get(index);
+        assertEquals("some/led/" + index + "/rgb", message.getTopic());
+        assertEquals(color, message.getPayload());
     }
 }
 
